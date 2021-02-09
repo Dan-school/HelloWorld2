966f7e742f02d59a36b000f24d1d9e887839ce7e
Daniel Lindberg <daniel.lindberg@maine.edu>
2021-02-09 14:53:11 -0500

    bob

--- hello.java
+++ hello.java
@@ -3,5 +3,6 @@ class Hello{
         System.out.println("her ye her ye! By order of the King, me, Derek, I, we do declare Dreamland is now a theocracy!");
         System.out.println("Hello, World!"); 
         System.out.println("Aliright bye");
+        System.out.println("hi, Bob");
     }
 }
