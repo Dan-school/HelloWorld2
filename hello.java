10fb96d09097e1fd1be56330a640e767077f3887
Daniel Lindberg <daniel.lindberg@maine.edu>
2021-02-09 14:45:05 -0500

    opening

--- hello.java
+++ hello.java
@@ -1,5 +1,6 @@
 class Hello{
     public static void main(String[] args) {
+        System.out.println("her ye her ye! By order of the King, me, Derek, I, we do declare Dreamland is now a theocracy!");
         System.out.println("Hello, World!"); 
     }
 }
